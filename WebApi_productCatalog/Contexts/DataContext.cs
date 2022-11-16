9a36c550c3a24618aded915219a7efacb4044531
Anna <annaelg@live.se>
2022-11-16 10:44:28 +0100

    Add project files.

---
+++ WebApi_productCatalog/Contexts/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_productCatalog.Models;
+
+namespace WebApi_productCatalog.Contexts
+{
+    public class DataContext: DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<ProductModel> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductModel>().ToContainer("Products").HasPartitionKey(x => x.PartionKey);
+        }
+    }
+}
---
+++ WebApi_productCatalog/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi_productCatalog.Models
+{
+    public class ProductModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public int ArticelNumber { get; set; } 
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = null!;
+
+        public string PartionKey => Id;
+
+    }
+}
